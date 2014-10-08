947c05b869584f3b92e776a080fef8b79b4a39ad
Robert Pitt <rpitt@rpitt-laptop-02>
2014-10-08 22:58:23 +0100

    Added extra methods, extra documentation and cleaned some variables up

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
  * @package 	api
  * @author		Robert Pitt <rpitt@centiq.co.uk>
  * @copyright 	GPL V2
- * @version		1.0.0
- * @link	 	https://centiq.co.uk
- * @since		1.0.0
+ * @version		1.0.1
+ * @link	 	https://centiq.co.uk/
  */
 
 /**
@@ -19,44 +18,45 @@ var request	= require("request"),
 	merge 	= require("merge");
 
 /**
- * Default configuration
- */
-var _default_config = {
-	/**
-	 * Token used for requests
-	 * @type {String}
-	 */
-	token : null,
-
-	/**
-	 * Secret used with the token for authorization
-	 * @type {String}
-	 */
-	secret : null,
-
-	/**
-	 * Target server
-	 * @type {String}
-	 */
-	server : null,
-
-	/**
-	 * protocol used for requests
-	 * @type {String}
-	 */
-	protocol : 'http',
-
-	/**
-	 * Port that sirportly is running on.
-	 * @type {Number}
-	 */
-	port : 80
-};
-
-/**
  * Sirportly API Class
  */
 function SirportlyAPI(config) {
+	/**
+	 * Default configuration
+	 * @type {Object}
+	 */
+	var _default_config = {
+		/**
+		 * Token used for requests
+		 * @type {String}
+		 */
+		token : null,
+
+		/**
+		 * Secret used with the token for authorization
+		 * @type {String}
+		 */
+		secret : null,
+
+		/**
+		 * Target server
+		 * @type {String}
+		 */
+		server : null,
+
+		/**
+		 * protocol used for requests
+		 * @type {String}
+		 */
+		protocol : 'http',
+
+		/**
+		 * Port that sirportly is running on.
+		 * @type {Number}
+		 */
+		port : 80
+	};
+	
 	/**
 	 * merge the configuration
 	 */
@@ -146,9 +146,18 @@ SirportlyAPI.prototype.request = function(uri, options, callback) {
 };
 
 /**
+ * Fetch the configuration object for this instance
+ * @return {Object} Configuration object
+ */
+SirportlyAPI.prototype.getConfig = function() {
+	return this._config;
+};
+
+/**
  * Fetch a specific ticket object
  * @param  {String}   ticket_reference Ticket Reference ID
  * @param  {Function} callback         Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/getting-all-ticket-properties
  */
 SirportlyAPI.prototype.ticket = function(ticket_reference, callback) {
 	this.request('/api/v2/tickets/ticket', {qs: {reference: ticket_reference}}, callback);
@@ -158,6 +167,8 @@ SirportlyAPI.prototype.ticket = function(ticket_reference, callback) {
  * Fetch a list of tickets
  * @param  {Number|Function}   page     Page or callback, page wil default to 1 if not present
  * @param  {Function} callback Callback
+ * @todo Implement sort_by and order
+ * @see http://sirportly.com/docs/api-specification/tickets/listing-all-tickets
  */
 SirportlyAPI.prototype.tickets = function(page, callback) {
 	if(typeof page == 'function'){ page = 1; callback = page;};
@@ -168,8 +179,9 @@ SirportlyAPI.prototype.tickets = function(page, callback) {
  * Create a new Ticket
  * @param  {Object}   params   Ticket Parameters
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/submitting-a-new-ticket
  */
-SirportlyAPI.prototype.create_ticket = function(params, callback) {
+SirportlyAPI.prototype.submit_ticket = function(params, callback) {
 	this.request('/api/v2/tickets/submit',{method: "POST", qs : params || {}}, callback);
 }
 
@@ -178,6 +190,7 @@ SirportlyAPI.prototype.create_ticket = function(params, callback) {
  * @param  {String}   reference Ticket Reference
  * @param  {Object}   updates   Updates
  * @param  {Function} callback  Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/posting-an-update
  */
 SirportlyAPI.prototype.post_update = function(reference, updates, callback) {
 	this.request('/api/v2/tickets/post_update',{method: "POST", qs: merge(updates || {}, {ticket: reference})}, callback);
@@ -188,6 +201,7 @@ SirportlyAPI.prototype.post_update = function(reference, updates, callback) {
  * @param  {String}   reference Ticekt Reference
  * @param  {Object}   updates   Updates for the ticket
  * @param  {Function} callback  Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/changing-ticket-properties
  */
 SirportlyAPI.prototype.update_ticket = function(reference, updates, callback) {
 	this.request('/api/v2/tickets/update', {method: "POST", qs: merge(updates || {}, {ticket: reference})}, callback);
@@ -198,6 +212,7 @@ SirportlyAPI.prototype.update_ticket = function(reference, updates, callback) {
  * @param  {String}   		reference   Ticket Reference
  * @param  {String|Number}  macro    Macro name or id
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/execute-a-macro
  */
 SirportlyAPI.prototype.run_macro = function(reference, macro, callback) {
 	this.request('/api/v2/tickets/macro', {qs: {ticket: reference, macro: macro}}, callback);
@@ -208,6 +223,7 @@ SirportlyAPI.prototype.run_macro = function(reference, macro, callback) {
  * @param {String}   reference   Ticket Reference
  * @param {Object}   params   	 parameters for the request
  * @param {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/add-a-follow-up
  */
 SirportlyAPI.prototype.add_follow_up = function(reference, params, callback) {
 	this.request('/api/v2/tickets/add_followup', {method: "POST", qs : merge(params || {}, {ticket: reference})}, callback);
@@ -216,67 +232,97 @@ SirportlyAPI.prototype.add_follow_up = function(reference, params, callback) {
 /**
  * Request Statuses
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/statuses
  */
 SirportlyAPI.prototype.statuses = function(callback) {
-	return this.request('/api/v2/objects/statuses', {}, callback);
+	this.request('/api/v2/objects/statuses', {}, callback);
 }
 
 /**
  * Request Priorities
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/priorities
  */
 SirportlyAPI.prototype.priorities = function(callback) {
-	return this.request('/api/v2/objects/priorities', {}, callback);
+	this.request('/api/v2/objects/priorities', {}, callback);
 }
 
 /**
  * Request Teams
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/teams
  */
 SirportlyAPI.prototype.teams = function(callback) {
-	return this.request('/api/v2/objects/teams', {}, callback);
+	this.request('/api/v2/objects/teams', {}, callback);
 }
 
 /**
  * Request Brands
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/brands
  */
 SirportlyAPI.prototype.brands = function(callback) {
-	return this.request('/api/v2/objects/brands', {}, callback);
+	this.request('/api/v2/objects/brands', {}, callback);
 }
 
 /**
  * Request Departments
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/departments
  */
 SirportlyAPI.prototype.departments = function(callback) {
-	return this.request('/api/v2/objects/departments', {}, callback);
+	this.request('/api/v2/objects/departments', {}, callback);
 }
 
 /**
  * Request Escalation Paths
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/escalation-paths
  */
 SirportlyAPI.prototype.escalation_paths = function(callback) {
-	return this.request('/api/v2/objects/escalation_paths', {}, callback);
+	this.request('/api/v2/objects/escalation_paths', {}, callback);
 }
 
 /**
  * Request SLA's
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/slas
  */
 SirportlyAPI.prototype.slas = function(callback) {
-	return this.request('/api/v2/objects/slas', {}, callback);
+	this.request('/api/v2/objects/slas', {}, callback);
 }
 
 /**
  * Request Filters
  * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/objects/filters
  */
 SirportlyAPI.prototype.filters = function(callback) {
-	return this.request('/api/v2/objects/filters', {}, callback);
+	this.request('/api/v2/objects/filters', {}, callback);
 }
 
+/**
+ * Create a new user
+ * @param  {String}   email    Email Address
+ * @param  {String}   first    Firstname
+ * @param  {String}   last     Lastname
+ * @param  {Object}   params   Other properties for the user
+ * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/users/create-new-user
+ */
+SirportlyAPI.prototype.create_user = function(email, first, last, params, callback) {
+	this.request('/api/v2/users/create',{method: "POST", qs: merge({email_address: email, first_name: first, last_name: last}, params)}, callback);
+}
+
+/**
+ * Execute a SPQL Query.
+ * @param  {String}   query    Query to execute
+ * @param  {Function} callback Callback
+ * @see http://sirportly.com/docs/api-specification/tickets/run-spql-query
+ */
+SirportlyAPI.prototype.spql = function(query, callback) {
+	this.request('/api/v2/tickets/spql', {method: "POST", qs: {spql: query}}, callback);
+}
 
 /**
  * Export the sirportly apiobject
